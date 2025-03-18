54e3c1a9484ea578ba24262ce8f8e92fb1e1286f
MaksimPeterburgskiy <maksim.peter777@gmail.com>
2025-03-18 17:36:43 -0400

    add new providers to main.dart

--- petwise/lib/main.dart
+++ petwise/lib/main.dart
@@ -6,7 +6,12 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:petwise/data/providers/user_provider.dart';
 import 'package:petwise/data/repositories/user_repo.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart'; // Remove this import
+import 'package:petwise/data/repositories/pet_repo.dart';
+import 'package:petwise/data/providers/pet_provider.dart';
+import 'package:petwise/data/repositories/pet_user_repo.dart';
+import 'package:petwise/data/providers/pet_user_provider.dart';
+import 'package:petwise/data/repositories/vet_user_repo.dart';
+import 'package:petwise/data/providers/vet_user_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,24 +32,41 @@ Future<void> main() async {
   // Initialize PocketBase with the API URL
   final pb = PocketBase(apiURl, authStore: store);
 
-  // get pb instance in any widget using provider
-  //import 'package:pocketbase/pocketbase.dart';
-  //import 'package:provider/provider.dart';
-  //final pb = Provider.of<PocketBase>(context);
-
-  // Check if the stored credentials are valid
-  //pb.authStore.isValid();
-
   runApp(
     MultiProvider(
       providers: [
+        // pocketbase provider
         Provider<PocketBase>.value(value: pb),
-        Provider<UserRepository>(
-          create: (_) => UserRepository.getInstance(pb),
-        ),
+
+        // user repository provider
+        Provider<UserRepository>(create: (_) => UserRepository.getInstance(pb),),
         ChangeNotifierProvider<UserProvider>(
           create: (context) => UserProvider(
             Provider.of<UserRepository>(context, listen: false),
+          ),
+        ),
+
+        //petUser repository provider
+        Provider<PetUserRepository>.value(value: PetUserRepository.getInstance(pb)),
+        ChangeNotifierProvider<PetUserProvider>(
+          create: (context) => PetUserProvider(
+            Provider.of<PetUserRepository>(context, listen: false),
+          ),
+        ),
+
+        // pet repository provider
+        Provider<PetRepository>.value(value: PetRepository.getInstance(pb)),
+        ChangeNotifierProvider<PetProvider>(
+          create: (context) => PetProvider(
+            Provider.of<PetRepository>(context, listen: false),
+          ),
+        ),
+
+        //vetUser repository provider
+        Provider<VetUserRepository>.value(value: VetUserRepository.getInstance(pb)),
+        ChangeNotifierProvider<VetUserProvider>(
+          create: (context) => VetUserProvider(
+            Provider.of<VetUserRepository>(context, listen: false),
           ),
         ),
       ],
