bb490833a9a67ae7710a300d477a6848b11326b7
Ryan Carroll <ryan.carroll421@gmail.com>
2025-03-18 17:37:26 -0400

    Properly tracks date when switching views

--- petwise/lib/ui/ui_calendar/calendar.dart
+++ petwise/lib/ui/ui_calendar/calendar.dart
@@ -1,7 +1,18 @@
 import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 var controller = EventController();
+
+var headerStyle = const HeaderStyle(
+  decoration: BoxDecoration(
+    color: Colors.blue,
+  ),
+  headerTextStyle: TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+  ),
+);
 
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
@@ -13,51 +24,59 @@ class Calendar extends StatefulWidget {
 class _CalendarState extends State<Calendar> {
   Widget? calendarView;
   Widget? buttonRow;
+  DateTime selectedDate = DateTime.now();
 
   @override
   void initState() {
     super.initState();
-    monthView(DateTime.now());
+    monthView(selectedDate);
   }
 
   void dayView(DateTime date) {
+    selectedDate = date;
     setState(() {
       calendarView = DayView(
         controller: controller,
-        initialDay: date,
+        initialDay: selectedDate,
+        headerStyle: headerStyle,
+        onPageChange: (date, page) => selectedDate = date,
       );
       buttonRow = Row(
         children: [
           ElevatedButton(
-            onPressed: () => monthView(date),
+            onPressed: () => monthView(selectedDate),
             child: const Text('<- Month'),
           ),
-          ElevatedButton(onPressed: () => weekView(date), child: const Text('Week')),
+          ElevatedButton(onPressed: () => weekView(selectedDate), child: const Text('Week')),
         ],
       );
     });
   }
 
   void weekView(DateTime date) {
+    selectedDate = date;
     setState(() {
       calendarView = WeekView(
         controller: controller,
-        initialDay: date,
+        initialDay: selectedDate,
         startDay: WeekDays.sunday,
+        onPageChange: (date, page) => selectedDate = date,
+        headerStyle: headerStyle,
       );
       buttonRow = Row(
         children: [
           ElevatedButton(
-            onPressed: () => monthView(date),
+            onPressed: () => monthView(selectedDate),
             child: const Text('<- Month'),
           ),
-          ElevatedButton(onPressed: () => dayView(date), child: const Text('Day')),
+          ElevatedButton(onPressed: () => dayView(selectedDate), child: const Text('Day')),
         ],
       );
     });
   }
 
   void monthView(DateTime date) {
+    selectedDate = date;
     setState(() {
       calendarView = MonthView(
         controller: controller,
@@ -71,9 +90,9 @@ class _CalendarState extends State<Calendar> {
         //   // Return your widget to display as month cell.
         //   return Container();
         // },
-        initialMonth: date,
+        initialMonth: selectedDate,
         cellAspectRatio: 1,
-        onPageChange: (date, pageIndex) => print("$date, $pageIndex"),
+        onPageChange: (date, pageIndex) => selectedDate = date,
         onCellTap: (events, date) => dayView(date),
         startDay: WeekDays.sunday, // To change the first day of the week.
         // This callback will only work if cellBuilder is null.
@@ -81,10 +100,11 @@ class _CalendarState extends State<Calendar> {
         onEventDoubleTap: (events, date) => print(events),
         onEventLongTap: (event, date) => print(event),
         onDateLongPress: (date) => print(date),
-        // headerBuilder: MonthHeader.hidden, // To hide month header
-        showWeekTileBorder: true, // To show or hide header border
+        showWeekTileBorder: false, // To hide header border
         hideDaysNotInMonth: true, // To hide days or cell that are not in current month
         useAvailableVerticalSpace: true,
+        headerStyle: headerStyle,
+        headerStringBuilder: (date, {secondaryDate}) => DateFormat.yMMMM().format(date),
       );
       buttonRow = Row(
         children: [
@@ -105,21 +125,21 @@ class _CalendarState extends State<Calendar> {
       ),
       body: Column(
         children: [
-        //   const Text('Debug Buttons:'),
-        //   Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        //     ElevatedButton(
-        //       onPressed: () => monthView(DateTime.now()),
-        //       child: const Text('Month View'),
-        //     ),
-        //     ElevatedButton(
-        //       onPressed: () => weekView(DateTime.now()),
-        //       child: const Text('Week View'),
-        //     ),
-        //     ElevatedButton(
-        //       onPressed: () => dayView(DateTime.now()),
-        //       child: const Text('Day View'),
-        //     ),
-        //   ]),
+          //   const Text('Debug Buttons:'),
+          //   Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          //     ElevatedButton(
+          //       onPressed: () => monthView(DateTime.now()),
+          //       child: const Text('Month View'),
+          //     ),
+          //     ElevatedButton(
+          //       onPressed: () => weekView(DateTime.now()),
+          //       child: const Text('Week View'),
+          //     ),
+          //     ElevatedButton(
+          //       onPressed: () => dayView(DateTime.now()),
+          //       child: const Text('Day View'),
+          //     ),
+          //   ]),
           buttonRow!,
           Expanded(
             child: calendarView!,
